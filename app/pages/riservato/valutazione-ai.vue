cf2cae0cc4049b375495a3edf8fd611b1b55ffb9
Galdrial <camerano.simo@gmail.com>
2026-09-02 17:20:31 +0200

    fix(speed-to-lead): allinea il Pixel al dataset dell'account pubblicitario

--- app/pages/riservato/valutazione-ai.vue
+++ app/pages/riservato/valutazione-ai.vue
@@ -38,7 +38,7 @@ function iniettaPixel() {
     n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
     t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
     document,'script','https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1591505489274762');
+    fbq('init', '930077189597503');
     fbq('track', 'PageView');
   `
   document.head.appendChild(script)
